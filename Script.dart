557e7dc2a74ca141cc870751031afab20fd605ff
darwin <darwinrobles160@gmail.com>
2023-04-13 18:37:06 -0500

    creando funcion circulo

--- Script.dart
+++ Script.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 void main() {
   List opcione = ['1. factorialo', '2. Circulo', '1. perimetro', '2. area'];
@@ -11,18 +12,36 @@ void main() {
       stdout.writeln("ingrese el numero a factoriar ");
       int n = int.parse(stdin.readLineSync().toString());
       stdout.writeln(factorial(n));
-      
+
       break;
     case "2":
-      //circulo();
+      stdout.writeln("ingrese el diametro del circulo ");
+      double n = double.parse(stdin.readLineSync().toString());
+      stdout.writeln("elija la operacion ");
+      stdout.writeln(opcione[2]);
+      stdout.writeln(opcione[3]);
+       int o= int.parse(stdin.readLineSync().toString());
+       stdout.writeln(circulo(n, o)); 
       break;
   }
 }
 
 int factorial(n) {
- if (n <= 0) {         
-      return 1; 
-   } else { 
-      return (n * factorial(n - 1));    
-   } 
+  if (n <= 0) {
+    return 1;
+  } else {
+    return (n * factorial(n - 1));
+  }
+}
+
+String circulo(n, o) {
+  double r = n / 2, t;
+  if (o == 1) {
+    t = pi * n;
+    return "el perimetro es: $t";
+  } else if (o == 2) {
+    t = pi * (r * r);
+    return "el area es: $t";
+  }
+  return "no seleciono ninguna de las dos opciones";
 }
